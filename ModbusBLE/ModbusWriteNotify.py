40a26475dcd9b167a2e43ce1a283c2650a6c1f36
Lagm0n <lagmon0303@gmail.com>
2022-03-23 15:56:49 +0900

    Modbus Ble
    데이터 요청, 데이터 받기

--- ModbusBLE/ModbusWriteNotify.py
+++ ModbusBLE/ModbusWriteNotify.py
@@ -1,5 +1,4 @@
 import asyncio
-from pickletools import bytes8
 from bleak import BleakClient
 
 address = "62:00:A1:10:00:5A"
@@ -8,23 +7,28 @@ notify_uuid = "0000ffe4-0000-1000-8000-00805f9b34fb"
 
 
 def notify_callback(sender: int, data: bytearray):
-    print('sender: ', sender, 'data: ', data)
+    print('sender: ', sender, 'data: ', data.hex())
 
 async def run(address, write, notify):    
     async with BleakClient(address) as client:
         print('connected')
-        services = await client.get_services()        
+        services = await client.get_services()
         for service in services:
             for characteristic in service.characteristics:
                 if characteristic.uuid == write:                       
                     if 'write' in characteristic.properties:
                         print('write data')
-                        await client.write_gatt_char(characteristic, bytes8(b'05030210002AC5EC'))   
+                        # w=bytearray('0x05, 0x03, 0x02, 0x10, 0x00, 0x2A, 0xC5, 0xEC', 'hex')
+                        # w=0x05030210002AC5EC
+                        w=bytearray(b'\x05\x03\x02\x10\x00\x2A\xC5\xEC')
+                        await client.write_gatt_char(characteristic, w)  
                 if characteristic.uuid == notify:
                     if 'notify' in characteristic.properties:
                         print('try to activate notify.')
-                        await client.start_notify(characteristic, notify_callback)
-    
+                        await client.start_notify(characteristic.uuid, notify_callback)
+                        await asyncio.sleep(5.0)
+                        # await client.stop_notify(characteristic.uuid)
+
     print('disconnect')
 
 loop = asyncio.get_event_loop()
--- ModbusBLE/ModbusWriteRead.py
+++ ModbusBLE/ModbusWriteRead.py
@@ -1,8 +1,9 @@
 import asyncio
 from bleak import BleakClient
 
-address = "8c:aa:b5:84:db:52"
-read_write_charcteristic_uuid = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+address = "62:00:A1:10:00:5A"
+write_uuid = "0000ffe9-0000-1000-8000-00805f9b34fb"
+read_uuid = "0000ffe6-0000-1000-8000-00805f9b34fb"
 
 async def run(address):    
     async with BleakClient(address) as client:
@@ -10,14 +11,16 @@ async def run(address):
         services = await client.get_services()        
         for service in services:
             for characteristic in service.characteristics:
-                if characteristic.uuid == read_write_charcteristic_uuid:                       
+                if characteristic.uuid == write_uuid:                       
                     # 데이터 쓰기전 원래 데이터 읽기
                     if 'read' in characteristic.properties:
                         read_data = await client.read_gatt_char(characteristic)
                         print('read before writing: ', read_data)
                     # 데이터 쓰기
                     if 'write' in characteristic.properties:
-                        await client.write_gatt_char(characteristic, bytes(b'hello world'))                        
+                        w=bytearray(b'\x05\x03\x02\x10\x00\x2A\xC5\xEC')
+                        await client.no
+                        await client.write_gatt_char(characteristic, w)                        
                     # 쓰기 이후 데이터 읽기
                     if 'read' in characteristic.properties:
                         read_data = await client.read_gatt_char(characteristic)
