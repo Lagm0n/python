a29d1b0e560438c3b6c84d38b866ee204aafe3fd
Lagm0n <100834605+Lagm0n@users.noreply.github.com>
2022-06-07 17:07:52 +0900

    crc test json request test

--- ModbusBLE/crc.py
+++ ModbusBLE/crc.py
@@ -14,9 +14,13 @@ g3=setData('05 03 04 52 00 03')
 g4=setData('05 03 05 52 00 03')
 g5=setData('05 03 06 52 00 03')
 
+g6=setData('05 03 05 10 00 2A')
+
+
 print(g0.hex())
 print(g1.hex())
 print(g2.hex())
 print(g3.hex())
 print(g4.hex())
 print(g5.hex())
+print(g6.hex())
